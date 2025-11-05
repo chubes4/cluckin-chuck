4fec179d693c35c338ae9a7ae107d5ae893ed60a
Chris Huber <chubes4@users.noreply.github.com>
2025-11-04 23:04:04 -0600

    Initial commit

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Cluckin Chuck Theme Functions
+ *
+ * @package CluckinChuck
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function cluckin_chuck_setup() {
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'editor-styles' );
+	add_theme_support( 'wp-block-styles' );
+	add_theme_support( 'responsive-embeds' );
+	add_theme_support( 'align-wide' );
+	add_theme_support( 'custom-line-height' );
+	add_theme_support( 'custom-spacing' );
+	add_theme_support( 'custom-units' );
+}
+add_action( 'after_setup_theme', 'cluckin_chuck_setup' );
+
+function cluckin_chuck_enqueue_styles() {
+	wp_enqueue_style(
+		'cluckin-chuck-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'cluckin_chuck_enqueue_styles' );

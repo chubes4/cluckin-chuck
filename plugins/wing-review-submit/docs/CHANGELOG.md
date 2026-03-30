091c63c8afd228ac35a380d776a22b9b82448b54
Chris Huber <chubes4@users.noreply.github.com>
2026-03-30 04:46:15 +0000

    release: v0.1.5

--- plugins/wing-review-submit/docs/CHANGELOG.md
+++ plugins/wing-review-submit/docs/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [0.1.5] - 2026-03-30
+
+### Added
+- auto-fill reviewer identity from logged-in user account
+
 ## [0.1.4] - 2026-03-30
 
 ### Added
--- plugins/wing-review-submit/wing-review-submit.php
+++ plugins/wing-review-submit/wing-review-submit.php
@@ -3,7 +3,7 @@
  * Plugin Name: Wing Review Submit
  * Plugin URI: https://chubes.net
  * Description: Frontend submission form block for new wing locations and reviews
- * Version: 0.1.4
+ * Version: 0.1.5
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Author: Chris Huber
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WING_REVIEW_SUBMIT_VERSION', '0.1.4' );
+define( 'WING_REVIEW_SUBMIT_VERSION', '0.1.5' );
 define( 'WING_REVIEW_SUBMIT_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WING_REVIEW_SUBMIT_URL', plugin_dir_url( __FILE__ ) );
 
